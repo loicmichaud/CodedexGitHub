fa1d601e4d27225d1302ec9f83a8f9ac3fd88c05
Loïc MICHAUD <l.michaud@SMP-MOULES.LOCAL>
2025-10-22 08:30:16 +0200

    ajout .txt

---
+++ files.py
@@ -0,0 +1,6 @@
+file = open('diaries.txt', 'w', encoding='UTF-16')
+
+
+file.write("Je ne souhaite pas rester toute ma vie à SMP car le job ne me convient pas")
+file.write("Je préfère être à Dijon")
+file.write("j'en ai marre")
